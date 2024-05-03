3b0e935412d9f1aeae7539c190a4c81d49bbb8f6
Angel Senra <git@angelsenra.com>
2024-05-03 08:58:43 +0200

    Export UUID in compat module (#43)

--- python/uuid_utils/compat/__init__.py
+++ python/uuid_utils/compat/__init__.py
@@ -1,4 +1,4 @@
-import uuid
+from uuid import UUID
 
 import uuid_utils
 
@@ -8,24 +8,24 @@ def uuid1(node=None, clock_seq=None):
     If 'node' is not given, getnode() is used to obtain the hardware
     address.  If 'clock_seq' is given, it is used as the sequence number;
     otherwise a random 14-bit sequence number is chosen."""
-    return uuid.UUID(int=uuid_utils.uuid1(node, clock_seq).int)
+    return UUID(int=uuid_utils.uuid1(node, clock_seq).int)
 
 
 def uuid3(namespace, name):
     """Generate a UUID from the MD5 hash of a namespace UUID and a name."""
     namespace = uuid_utils.UUID(namespace.hex) if namespace else namespace
-    return uuid.UUID(int=uuid_utils.uuid3(namespace, name).int)
+    return UUID(int=uuid_utils.uuid3(namespace, name).int)
 
 
 def uuid4():
     """Generate a random UUID."""
-    return uuid.UUID(int=uuid_utils.uuid4().int)
+    return UUID(int=uuid_utils.uuid4().int)
 
 
 def uuid5(namespace, name):
     """Generate a UUID from the SHA-1 hash of a namespace UUID and a name."""
     namespace = uuid_utils.UUID(namespace.hex) if namespace else namespace
-    return uuid.UUID(int=uuid_utils.uuid5(namespace, name).int)
+    return UUID(int=uuid_utils.uuid5(namespace, name).int)
 
 
 def uuid6(node=None, timestamp=None):
@@ -33,14 +33,14 @@ def uuid6(node=None, timestamp=None):
     This is similar to version 1 UUIDs,
     except that it is lexicographically sortable by timestamp.
     """
-    return uuid.UUID(int=uuid_utils.uuid6(node, timestamp).int)
+    return UUID(int=uuid_utils.uuid6(node, timestamp).int)
 
 
 def uuid7(timestamp=None):
     """Generate a version 7 UUID using a time value and random bytes."""
-    return uuid.UUID(int=uuid_utils.uuid7(timestamp).int)
+    return UUID(int=uuid_utils.uuid7(timestamp).int)
 
 
 def uuid8(bytes):
     """Generate a custom UUID comprised almost entirely of user-supplied bytes.."""
-    return uuid.UUID(bytes=uuid_utils.uuid8(bytes).bytes)
+    return UUID(bytes=uuid_utils.uuid8(bytes).bytes)
