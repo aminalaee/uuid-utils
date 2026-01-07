5b47391700ced8bf5f79e97c2cc44ac5e22d858a
Amin Alaee <amin.alaee.zaviyeh@postnl.nl>
2026-01-07 08:46:25 +0100

    update typehints

--- python/uuid_utils/compat/__init__.pyi
+++ python/uuid_utils/compat/__init__.pyi
@@ -14,13 +14,7 @@ from uuid import (
     getnode,
 )
 
-from typing_extensions import TypeAlias
-
-# Because UUID has properties called int and bytes we need to rename these temporarily.
-_Int: TypeAlias = int
-_Bytes: TypeAlias = bytes
-
-def uuid1(node: _Int | None = None, clock_seq: _Int | None = None) -> UUID:
+def uuid1(node: int | None = None, clock_seq: int | None = None) -> UUID:
     """Generate a UUID from a host ID, sequence number, and the current time.
     If 'node' is not given, getnode() is used to obtain the hardware
     address.  If 'clock_seq' is given, it is used as the sequence number;
@@ -50,18 +44,18 @@ else:
         """Generate a UUID from the SHA-1 hash of a namespace UUID and a name."""
         ...
 
-def uuid6(node: _Int | None = None, timestamp: _Int | None = None) -> UUID:
+def uuid6(node: int | None = None, timestamp: int | None = None) -> UUID:
     """Generate a version 6 UUID using the given timestamp and a host ID.
     This is similar to version 1 UUIDs,
     except that it is lexicographically sortable by timestamp.
     """
     ...
 
-def uuid7(timestamp: _Int | None = None, nanos: _Int | None = None) -> UUID:
+def uuid7(timestamp: int | None = None, nanos: int | None = None) -> UUID:
     """Generate a version 7 UUID using a time value and random bytes."""
     ...
 
-def uuid8(bytes: _Bytes) -> UUID:
+def uuid8(bytes: bytes) -> UUID:
     """Generate a custom UUID comprised almost entirely of user-supplied bytes.."""
     ...
 
